d183345532d81b6160a688f22fb19ad8edb8c361
kelsingazin <yerassyl.kelsingazin@gmail.com>
2020-05-14 21:07:22 +0600

    make it better

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/angular-diploma'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('*', function (req, res){
+  res.sendFile(path.join(__dirname + '/dist/angular-diploma/index.html'));
+});
+
+console.log('Console listening');
