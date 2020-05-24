56022c89afc2599d2544adfe7b785e148a45939b
kelsingazin <yerassyl.kelsingazin@gmail.com>
2020-05-24 23:12:49 +0600

    URL changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://127.0.0.1:8000'
-  apiUrl: 'https://tourismkz.herokuapp.com'
+  apiUrl: 'https://tourismera.herokuapp.com'
 };
 
 /*
