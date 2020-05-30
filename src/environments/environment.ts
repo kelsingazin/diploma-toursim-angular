18b3aebebd8b0db1b68cdcc5f014b495116443c5
kelsingazin <yerassyl.kelsingazin@gmail.com>
2020-05-31 03:14:49 +0600

    Heroku setting added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://127.0.0.1:8000'
-  apiUrl: 'https://tourismera.herokuapp.com'
+  apiUrl: 'http://127.0.0.1:8000'
+  // apiUrl: 'https://tourismera.herokuapp.com'
 };
 
 /*
